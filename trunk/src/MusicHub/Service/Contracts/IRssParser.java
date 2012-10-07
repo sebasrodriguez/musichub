66cc92b2e0a58b42279e40d284d4b7fc726dc295
sebasrodriguez@gmail.com <sebasrodriguez@gmail.com@fc01f1ed-7c45-6fc5-7dc7-76816ad19df2>
2012-10-07 02:28:11 +0000

    Se implemento el metodo de obtener un listado de items rss de un canal

--- trunk/src/MusicHub/Service/Contracts/IRssParser.java
+++ trunk/src/MusicHub/Service/Contracts/IRssParser.java
@@ -2,4 +2,6 @@ package MusicHub.Service.Contracts;
 
 public interface IRssParser {
 
+	String getImageFromRssContent(String rssContent);
+
 }
--- trunk/src/MusicHub/Service/RssParser.java
+++ trunk/src/MusicHub/Service/RssParser.java
@@ -1,7 +1,23 @@
 package MusicHub.Service;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import MusicHub.Service.Contracts.IRssParser;
 
 public class RssParser implements IRssParser {
+	
+	private static final Pattern IMG_PATTERN = Pattern.compile(
+            "<img\\s+.*?(?:src\\s*=\\s*(?:'|\")(.*?)(?:'|\"))(.*?)/>", 
+            Pattern.DOTALL|Pattern.CASE_INSENSITIVE);
+
+	@Override
+	public String getImageFromRssContent(String rssContent) {
+		Matcher m = IMG_PATTERN.matcher(rssContent);
+		while (m.find()) {
+			return m.group(1);			
+		}
+		return "";
+	}
 
 }
