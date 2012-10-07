1fca6a2750056a425dca01cf24b758c5a61f7550
dbusca@gmail.com <dbusca@gmail.com@fc01f1ed-7c45-6fc5-7dc7-76816ad19df2>
2012-10-07 19:01:10 +0000

    ajustes en BasicContainer

--- trunk/src/MusicHub/UI/BasicContainer.java
+++ trunk/src/MusicHub/UI/BasicContainer.java
@@ -4,10 +4,14 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Image;
 
 import org.havi.ui.HContainer;
+import org.havi.ui.HIcon;
 import org.havi.ui.HScene;
 import org.havi.ui.HStaticText;
+
+import java.awt.Toolkit;
 
 import MusicHub.Util.Conf;
 
@@ -15,6 +19,8 @@ public class BasicContainer extends HContainer {
 	
 	private HScene subScene;
 	private HStaticText titleText;
+	private HIcon icon;
+	private Image img;
 	
 	public BasicContainer(HScene scene, String title){
 		
@@ -25,9 +31,17 @@ public class BasicContainer extends HContainer {
 		this.titleText.setBounds(0, 0, this.getWidth(), 60);
 		this.titleText.setFont(new Font(Conf.getFontName(), Font.PLAIN, Conf.getFontSize()));
 		this.titleText.setHorizontalAlignment((int) HStaticText.LEFT_ALIGNMENT);		
-		this.titleText.setForeground(Color.WHITE);				
+		this.titleText.setForeground(Color.WHITE);	
 		
-		this.add(titleText);
+		img=Toolkit.getDefaultToolkit().getImage("../assets/logo.gif");
+		icon= new HIcon(img,10,10,250,250);
+		icon.setSize(126, 56);
+		
+		this.add(icon);
+		//this.add(titleText);
+		
+
+		
 	}
 	
 	@Override
