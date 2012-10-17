7657efa2c62bca6b06200d9593f8cbfaa79ff166
sebasrodriguez@gmail.com <sebasrodriguez@gmail.com@fc01f1ed-7c45-6fc5-7dc7-76816ad19df2>
2012-10-17 01:25:14 +0000

    Fix de menu e implementacion de boton arriba.
    Se removieron clases no utilizadas para simplificar la lectura del codigo.
    
    Se mejoro la implementacion del menu y se hizo mas generico.

--- trunk/src/MusicHub/UI/MainView.java
+++ trunk/src/MusicHub/UI/MainView.java
@@ -1,11 +1,7 @@
 package MusicHub.UI;
 
-
-import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
-import java.awt.LayoutManager;
 import java.awt.Toolkit;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
@@ -13,66 +9,59 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.LinkedList;
 import java.util.List;
-
-import org.havi.ui.HContainer;
 import org.havi.ui.HIcon;
-import org.havi.ui.HScene;
 
-public class MainView extends BasicContainer implements KeyListener, FocusListener {
-	
-	private HScene subScene;
+public class MainView extends BasicContainer implements KeyListener,
+		FocusListener {
+
+	private static final long serialVersionUID = 1L;
 	private UMenuScrollable mainMenu;
 	private Image img;
-	private HIcon ico;
-	
-	public MainView(String title){
+
+	public MainView(String title) {
 		super(title);
-		
-		
-		
-		img=Toolkit.getDefaultToolkit().getImage("../imgs/arrow_img3.jpg");		
-		//ico= new HIcon(img,0,0,800,500);		
-		
-		UOptionItem item= new UOptionItem(new HIcon(img,0,0,800,500), "Opcion 1");	
-		item.setName("item 0");
-		UOptionItem item2= new UOptionItem(new HIcon(img,0,0,800,500), "Opcion 2");
-		item2.setName("item 1");
-		UOptionItem item3= new UOptionItem(new HIcon(img,0,0,800,500), "Opcion 3");
-		item3.setName("item 2");
-		UOptionItem item4= new UOptionItem(new HIcon(img,0,0,800,500), "Opcion 4");
-		item4.setName("item 3");		
-		
-		List itemList=new LinkedList();
+
+		img = Toolkit.getDefaultToolkit().getImage("../imgs/arrow_img3.jpg");
+
+		UOptionItem item = new UOptionItem(new HIcon(img, 0, 0, 800, 500),
+				"Canales");
+		UOptionItem item2 = new UOptionItem(new HIcon(img, 0, 0, 800, 500),
+				"Facebook");
+		UOptionItem item3 = new UOptionItem(new HIcon(img, 0, 0, 800, 500),
+				"Twitter");
+		UOptionItem item4 = new UOptionItem(new HIcon(img, 0, 0, 800, 500),
+				"Ayuda");
+
+		List<UOptionItem> itemList = new LinkedList<UOptionItem>();
 		itemList.add(item);
 		itemList.add(item2);
 		itemList.add(item3);
-		itemList.add(item4);			
-		
-		mainMenu= new UMenuScrollable(itemList);
-		mainMenu.setBounds(150, 60, 500, 500);		
-		
-		this.add(mainMenu);	
-		//scene.add(this);
+		itemList.add(item4);
+
+		mainMenu = new UMenuScrollable(itemList, 150, 150);
+
+		this.add(mainMenu);
+		// scene.add(this);
 	}
 
 	@Override
 	public void focusGained(FocusEvent e) {
 		// TODO Auto-generated method stub
 		System.out.println("mainView focus");
-		//mainMenu.requestFocus();
+		// mainMenu.requestFocus();
 	}
 
 	@Override
 	public void focusLost(FocusEvent e) {
 		// TODO Auto-generated method stub
 		System.out.println("main lost");
-		
+
 	}
 
 	@Override
 	public void paint(Graphics g) {
 		// TODO Auto-generated method stub
-		
+
 		mainMenu.requestFocus();
 		super.paint(g);
 	}
@@ -80,20 +69,19 @@ public class MainView extends BasicContainer implements KeyListener, FocusListen
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
-	
+
 }
--- trunk/src/MusicHub/UI/UMenuScrollable.java
+++ trunk/src/MusicHub/UI/UMenuScrollable.java
@@ -1,147 +1,123 @@
 package MusicHub.UI;
+
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
-
-import org.havi.ui.HComponent;
 import org.havi.ui.HContainer;
-
 import MusicHub.Util.Conf;
 
+public class UMenuScrollable extends HContainer implements KeyListener,
+		FocusListener {
 
-
-
-public class UMenuScrollable extends HContainer implements KeyListener, FocusListener {
-
-	private List listItems;
-	private int itemHPos=0;
-	private HContainer wrapper;
+	private static final long serialVersionUID = 1L;
+	private List<UOptionItem> listItems;
 	private HContainer menuContainer;
-	private int selectedItemIndex=0;
+	private int selectedItemIndex = 0;
 	private UOptionItem itemSelected;
-	// TODO Agregar el scroll como atributo
-	
-	public UMenuScrollable(){
-		
-		listItems= new LinkedList();
-		
-		setSubContainers();
-		
+
+	private int getMenuHeight(){
+		return this.getNumItems() * Conf.getItemHeight();
 	}
-	
-	//Constructor que acepta una lista de UOptionItem
-	public UMenuScrollable(List items){		
-		this.listItems=items;
+
+	// Constructor que acepta una lista de UOptionItem
+	public UMenuScrollable(List<UOptionItem> items, int x, int y) {
+		this.listItems = items;
 		setSubContainers();
 		showItems();
+		this.setBounds(x, y, Conf.getMenuWidth(), this.getMenuHeight());
 	}
-	
-	
-	private int calcHeight(){
-		
-		//TODO Conf.getItemsHeight
-		return Conf.getItemHeight() * getNumItems();
-	}
-	
-	private void setSubContainers(){
-		menuContainer= new HContainer(0,0,this.getWidth(), this.getHeight());
-		menuContainer.setSize(250,204);
-		wrapper= new HContainer();
+
+	private void setSubContainers() {
+		menuContainer = new HContainer(0, 0, Conf.getMenuWidth(), this.getMenuHeight());		
 		addKeyListener(this);
 		addFocusListener(this);
 	}
-	
-	public void addOption (UOptionItem opt){	
+
+	public void addOption(UOptionItem opt) {
 		this.listItems.add(opt);
-	}	
-	
-	public int getNumItems(){
+	}
+
+	public int getNumItems() {
 		return this.listItems.size();
 	}
-	
-	public void showItems(){
-		
-		Iterator it=listItems.iterator();
-		int index=0;
-	
-		while(it.hasNext()){						
-			
-			UOptionItem aux=(UOptionItem)it.next();
-			itemHPos+=aux.getHeight();
-			aux.setLocation(0,itemHPos);
-			
-			//System.out.println(String.valueOf(aux.getName()));
-			this.menuContainer.add(aux,index);
-			
-			index++;			
+
+	public void showItems() {
+		Iterator<UOptionItem> it = listItems.iterator();
+		int index = 0;
+		int itemVPos = 0;
+
+		while (it.hasNext()) {
+			UOptionItem aux = it.next();
+			aux.setLocation(0, itemVPos);
+			itemVPos += aux.getHeight();
+			this.menuContainer.add(aux, index);
+			index++;
 		}
-		
-		//Indico que la primera opcion es la seleccionada
-		((UOptionItem)this.listItems.get(0)).setSelected(true);
-		itemSelected=(UOptionItem)this.listItems.get(0); 
-				
-		menuContainer.setVisible(true);		
-		this.add(menuContainer);	
-		
 
+		// Indico que la primera opcion es la seleccionada
+		((UOptionItem) this.listItems.get(0)).setSelected(true);
+		itemSelected = (UOptionItem) this.listItems.get(0);
+
+		menuContainer.setVisible(true);
+		this.add(menuContainer);
 	}
 
-	
-	//KeyListeners
+	// KeyListeners
 	public void keyPressed(KeyEvent e) {
-		// TODO logica para bajar o subir en el menu scroleable
-		
-		//flecha abajo
-		if  (e.getKeyCode() == 40){
-			
+		// flecha abajo y arriba		
+		switch (e.getKeyCode()) {
+		case 10:
+			System.out.println("ok");
+			break;
+		case 40:
+		case 38:
 			itemSelected.setSelected(false);
-			UOptionItem next=null;
-			selectedItemIndex++;
-			
-			if(selectedItemIndex<menuContainer.getComponentCount()){				
-				next=((UOptionItem)menuContainer.getComponent(selectedItemIndex));
+			UOptionItem next = null;
+			if (e.getKeyCode() == 40) {
+				selectedItemIndex++;
+			} else {
+				selectedItemIndex--;
 			}
-			else{
-				selectedItemIndex=0;
-				next=((UOptionItem)menuContainer.getComponent(selectedItemIndex));
-			}
-			
-			next.setSelected(true);
-			itemSelected=next;		//Guardo una referencia a la opcion seleccionada		
-			
-			System.out.println(next.getName());
-			
-		
-		 }  
-		
-	}
 
+			if (selectedItemIndex < 0) {
+				selectedItemIndex = menuContainer.getComponentCount() - 1;
+			}
+
+			if (selectedItemIndex < menuContainer.getComponentCount()) {
+				next = ((UOptionItem) menuContainer
+						.getComponent(selectedItemIndex));
+			} else {
+				selectedItemIndex = 0;
+				next = ((UOptionItem) menuContainer
+						.getComponent(selectedItemIndex));
+			}
+
+			next.setSelected(true);
+			itemSelected = next;
+			break;
+		}
+	}
 
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
-	}
 
+	}
 
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
-	}
 
+	}
 
 	public void focusGained(FocusEvent arg0) {
-		System.out.println("menu focus");		
-		
-	}
+		System.out.println("menu focus");
 
+	}
 
 	public void focusLost(FocusEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
