b57c082a3e2863132d9704b2da72c50f6eeb8031
sebasrodriguez@gmail.com <sebasrodriguez@gmail.com@fc01f1ed-7c45-6fc5-7dc7-76816ad19df2>
2012-10-20 18:12:50 +0000

    Refactor y bugfix de menu

--- src/MusicHub/UI/ChannelsView.java
+++ src/MusicHub/UI/ChannelsView.java
@@ -1,35 +1,16 @@
 package MusicHub.UI;
 
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Graphics;
+import MusicHub.UI.Contracts.ISelectedOption;
 
-import org.havi.ui.HTextButton;
+public class ChannelsView extends BasicContainer implements ISelectedOption{
 
-import MusicHub.Util.Conf;
-
-public class ChannelsView extends BasicContainer{
-
-	private static final long serialVersionUID = 1L;
-	private HTextButton button;
+	private static final long serialVersionUID = 1L;	
 	
 	public ChannelsView(){
-		this.setBounds(10, 0, 500, 500);
-		button = new HTextButton("test");
-		button.setBounds(0, 0, this.getWidth(), Conf.getItemHeight());
-		button.setForeground(Color.white);
-		button.setFont(new Font(Conf.getFontName(), Font.BOLD, Conf
-				.getFontSize()));
 		
-		this.add(button);		
-		this.popToFront(button);
-	}
-	
-	@Override
-	public void paint(Graphics g) {		
-		g.setColor(Color.RED);
-		g.fillRect(0, 0, this.getWidth(), this.getHeight());
-		super.paint(g);
 	}
 
+	@Override
+	public void selectedOption(String option) {				
+	}
 }
