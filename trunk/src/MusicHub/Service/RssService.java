ade2b3e24e50aa91e033eb2df9b161770287d2b2
sebasrodriguez@gmail.com <sebasrodriguez@gmail.com@fc01f1ed-7c45-6fc5-7dc7-76816ad19df2>
2012-10-30 23:31:11 +0000

    Se cambio la libreria de lectura de Rss por una que funciona con el emulador

--- trunk/src/MusicHub/Service/RssService.java
+++ trunk/src/MusicHub/Service/RssService.java
@@ -4,15 +4,14 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.horrabin.horrorss.RssItemBean;
+import org.horrabin.horrorss.RssParser;
+
 import MusicHub.Application.ServiceLocator;
 import MusicHub.DataTypes.RssFeed;
 import MusicHub.DataTypes.RssItem;
 import MusicHub.Service.Contracts.IRssParser;
 import MusicHub.Service.Contracts.IRssService;
-import com.sun.syndication.feed.synd.SyndEntryImpl;
-import com.sun.syndication.feed.synd.SyndFeed;
-import com.sun.syndication.io.SyndFeedInput;
-import com.sun.syndication.io.XmlReader;
 
 public class RssService implements IRssService {
 
@@ -20,28 +19,26 @@ public class RssService implements IRssService {
 	public List<RssItem> getRssItems(RssFeed feed) {
 		List<RssItem> items = new ArrayList<RssItem>();
 		IRssParser parser = ServiceLocator.getRssParser();
-		
-		try {
-			URL feedUrl = new URL(feed.getUrl());
-			SyndFeedInput input = new SyndFeedInput();
-			SyndFeed newFeed = input.build(new XmlReader(feedUrl));
+		org.horrabin.horrorss.RssParser rss = new RssParser();
 
-			List<SyndEntryImpl> feeds = newFeed.getEntries();
+		try {
+			org.horrabin.horrorss.RssFeed rssFeed = rss.load(feed.getUrl());
 			RssItem item = null;
-			for (SyndEntryImpl syndFeed : feeds) {
+
+			List<RssItemBean> rssItems = rssFeed.getItems();
+			for (int i = 0; i < rssItems.size(); i++) {
+				RssItemBean rssItem = rssItems.get(i);
+				
 				item = new RssItem();
-				
-				item.setTitle(syndFeed.getTitle());
-				item.setContent(syndFeed.getDescription().getValue());
-				item.setDate(syndFeed.getPublishedDate());
-				item.setImageUrl(parser.getImageFromRssContent(item.getContent()));
-				// TODO: cargar votos de servicio
+				item.setTitle(rssItem.getTitle());
+				item.setContent(rssItem.getDescription());
+				item.setDate(rssItem.getPubDate());
+				item.setImageUrl(parser.getImageFromRssContent(rssItem.getDescription()));
 				item.setVotes(66);
-				
-				items.add(item);
 			}
-		} catch (Exception e) {
-			e.printStackTrace();
+		}
+		catch (Exception ex) {
+			ex.printStackTrace();
 		}
 
 		return items;
